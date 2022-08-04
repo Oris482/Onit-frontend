015a35b4dd7171bcc2b9a5a83b44653b9fea7106
Eomhyunjun <zzp1318@gmail.com>
2022-08-04 17:11:23 +0900

    feat: 멀티페이지 css 1차 개선

--- src/components/PublishingPage/MultiPage/MultiHeader.js
+++ src/components/PublishingPage/MultiPage/MultiHeader.js
@@ -20,7 +20,7 @@ function MultiHeader({ multiData, pageUrl }) {
       const navList = pages.map((page, i) => {
         const key = i + 1;
         return (
-          <li key={key} css={[pureList]}>
+          <li key={key} css={[pureList, navListStyles]}>
             <Link
               css={[pureButton, selected === page.singlePageUrl && underline]}
               to={`/${myInfo.url}/${url}/${page.singlePageUrl}`}
@@ -38,10 +38,10 @@ function MultiHeader({ multiData, pageUrl }) {
   return (
     <header>
       <div css={displayCenter}>
-        <h1>{title}</h1>
+        <h1 css={titleStyles}>{title}</h1>
       </div>
       <div css={displayCenter}>
-        <ul>{navLists}</ul>
+        <ul css={navGap}>{navLists}</ul>
       </div>
     </header>
   );
@@ -54,21 +54,59 @@ const displayCenter = css`
   justify-content: center;
 `;
 
+const titleStyles = css`
+  height: 33px;
+  margin: 44.22px 77.88px 35.64px;
+  object-fit: contain;
+  font-family: SUIT;
+  font-size: 26.4px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: var(--black);
+`;
+
 const pureList = css`
   list-style: none;
   float: left;
   margin: 10px;
 `;
 
+const navListStyles = css`
+  height: 16.5px;
+  margin: 0 0 5.94px;
+  object-fit: contain;
+  font-family: SUIT;
+  font-size: 13.2px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+`;
+
 const pureButton = css`
   padding: 0;
   border: 0;
   outline: 0;
+  padding-bottom: 2.54px;
   &:hover {
-    text-decoration: underline;
+    border-bottom: solid 1.5px #fc3d1d;
+    color: #fc3d1d;
   }
 `;
 
+const navGap = css`
+  display: flex;
+  gap: 26.4px;
+  margin-bottom: 31.68px;
+`;
+
 const underline = css`
-  text-decoration: underline;
+  border-bottom: solid 1.5px;
+  color: #fc3d1d;
 `;
